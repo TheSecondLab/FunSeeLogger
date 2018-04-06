cc95e7d92a1b2089a9ed84e5c0542fd1aebbd577
MeatPieYan <robinyzg@hotmail.com>
2018-04-06 16:28:28 +0800

    initial verison

--- index.js
+++ index.js
@@ -1,34 +1,7 @@
-// const winston = require('winston');
+const FunSeeLogger = require('./lib/Log');
+const FunSeeLoggerMid = require('./lib/LogMid');
 
-// var logger = new (winston.Logger)({
-//   transports: [
-//     new (winston.transports.File)({
-//       name: 'info-file',
-//       filename: 'temp/filelog-info.log',
-//       level: 'info'
-//     }),
-//     new (winston.transports.File)({
-//       name: 'error-file',
-//       filename: 'temp/filelog-error.log',
-//       level: 'error',
-//       handleExceptions: true
-//     })
-//   ]
-// });
-
-// const logger = winston.createLogger({
-//   transports: [
-//     new winston.transports.Console({ handleExceptions: true })
-//   ]
-// });
-
-// throw new Error('Hello, winston!');
-
-// logger.info('hello')
-// logger.error('error')
-
-const Log = require('./lib/Log');
-
-const log = new Log();
-log.info("test123");
-log.error('error123')
+module.exports = {
+  FunSeeLogger,
+  FunSeeLoggerMid
+};
